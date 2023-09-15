1809c87612065c39698242640286c4a3321782a1
AngusGMorrison <github@angus-morrison.com>
2023-09-15 11:08:21 +0100

    Generate dotenv to include host-specific variables

--- scripts/generate_dev_data_dir.sh
+++ scripts/generate_dev_data_dir.sh
@@ -5,18 +5,18 @@ set -eou pipefail
 echo "Generating development data directory..."
 
 echo "Checking for existence of host data directory '${REALWORLD_HOST_DATA_DIR}'..."
-if [ -d ${REALWORLD_HOST_DATA_DIR} ]; then
+if [ -d "${REALWORLD_HOST_DATA_DIR}" ]; then
   echo "Host data directory already exists. Skipping generation."
   echo
   exit 0
 fi
 
 echo "Creating host data directory..."
-mkdir ${REALWORLD_HOST_DATA_DIR}
+mkdir "${REALWORLD_HOST_DATA_DIR}"
 echo "Host data directory created."
 
 echo "Generating development-only JWT RSA private key..."
-ssh-keygen -t rsa -b 4096 -m PEM -f ${REALWORLD_HOST_DATA_DIR}/${REALWORLD_JWT_RSA_PRIVATE_KEY_PEM_BASENAME} -N ""
+ssh-keygen -t rsa -b 4096 -m PEM -f "${REALWORLD_HOST_DATA_DIR}/${REALWORLD_JWT_RSA_PRIVATE_KEY_PEM_BASENAME}" -N ""
 echo "Development JWT key generated. This is gitignored by default. DO NOT COMMIT THIS KEY."
 
 echo "Data dependencies generated successfully at '${REALWORLD_HOST_DATA_DIR}!'"
--- scripts/generate_dev_dotenv.sh
+++ scripts/generate_dev_dotenv.sh
@@ -1,3 +1,19 @@
+#!/bin/bash
+
+# This script generates the dev.env file required to build and run
+# the application in development and CI. This allows us to dynamically
+# define variables based on the host environment, such as the
+# current working directory and the location of the Go compiler
+# cache.
+
+set -eou pipefail
+
+GOCACHE=$(go env GOCACHE)
+WORKDIR=$(pwd)
+
+echo "Generating dev.env..."
+
+cat > dev.env <<EOF
 # Environment variables used in the build and run tasks in development and test.
 # Overridden in production.
 # MUST NOT contain secrets.
@@ -8,9 +24,8 @@
 
 REALWORLD_APP_NAME=realworld
 
-# Relative path to the host data directory to be mounted into the app container.
-# Expanded to an absolute path in the relevant tasks.
-REALWORLD_HOST_DATA_DIR=data
+# Absolute path to the host data directory to be mounted into the app container.
+REALWORLD_HOST_DATA_DIR=$WORKDIR/data
 
 # Absolute path to the container directory where the host data directory will be
 # mounted.
@@ -25,6 +40,7 @@ REALWORLD_USER=65532
 ## Build ##
 ###########
 
+GOCACHE=$GOCACHE
 REALWORLD_CONTAINER_NAME=realworld
 REALWORLD_IMAGE_NAME=realworld
 
@@ -36,9 +52,7 @@ REALWORLD_DB_BASENAME=dev.db
 REALWORLD_ENABLE_STACK_TRACE=true
 REALWORLD_JWT_ISSUER="https://realworld.io"
 REALWORLD_JWT_RSA_PRIVATE_KEY_PEM_BASENAME=jwtRS256.key
+EOF
 
-
-
-
-
-
+echo "dev.env generated!"
+printf "Source and export it with\n\tset -a; source dev.env; set +a\n"
